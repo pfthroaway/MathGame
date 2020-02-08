8d74f7c0ad806cc8debe18771c1c23295ab307cc
pfthroaway <pfthroaway@mail.com>
2020-02-08 01:39:50 -0600

    Update AppData.cs

--- MathGame/AppData.cs
+++ MathGame/AppData.cs
@@ -6,6 +6,6 @@ namespace MathGame
     public static class AppData
     {
         internal static string Location = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MathGame");
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PF Software", "MathGame");
     }
 }
